6494d6b8b1e5d53a70e4a982e4bc1c5160f4eb64
Holence <Holence08@gmail.com>
2025-12-24 18:34:11 +0800

    .

--- src/bin/stress.rs
+++ src/bin/stress.rs
@@ -1,3 +1,4 @@
+use cursive::reexports::log::debug;
 use rand::{Rng, rngs::ThreadRng};
 use rustgo::backend::{Coord, Engine, Stone};
 
@@ -8,23 +9,31 @@ fn main() {
     let mut rng = rand::rng(); // a local handle to the generator
     let size = 128;
 
-    let mut e = Engine::new(size);
+    let mut engine = Engine::new(size);
     let mut stone = Stone::Black;
-    for _ in 0..1000000 {
-        let res = e.place_stone(
-            Coord::new(random_number(&mut rng, size), random_number(&mut rng, size)),
-            stone,
-        );
-        // if let Ok(res) = res {
-        //     if res.eaten.len() > 0 {
-        //         println!("{:?}", res.eaten);
-        //     }
-        // }
-        if stone == Stone::Black {
-            stone = Stone::White;
-        } else {
-            stone = Stone::Black;
+    let mut steps: usize = 0;
+    'outer: for _ in 0..1000000 {
+        for _ in 0..20 {
+            // 对于一种颜色，随机落子多次，如果都不成功，则结束对局
+            let res = engine.place_stone(
+                Coord::new(random_number(&mut rng, size), random_number(&mut rng, size)),
+                stone,
+            );
+            if res.is_ok() {
+                if stone == Stone::Black {
+                    stone = Stone::White;
+                } else {
+                    stone = Stone::Black;
+                }
+
+                let _ = debug!("{:?}", res);
+                debug!("{}", engine.board_string());
+                steps += 1;
+                continue 'outer;
+            }
         }
+        break 'outer;
     }
-    // println!("{}", e.board_string());
+    println!("{}", engine.board_string());
+    println!("steps: {}", steps);
 }
