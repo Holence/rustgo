cb938389b5d4a5ef46f4d13b8fb0a3618aa898d9
Holence <Holence08@gmail.com>
2026-04-25 13:34:19 +0800

    correctly handle connect/disconnect in GUI

--- crates/gui/src/main.rs
+++ crates/gui/src/main.rs
@@ -2,7 +2,6 @@ use server::common::{ClientId, DownlinkMessage, ReqId, RoomId, UplinkMessage};
 use tokio::{
     io::{AsyncBufReadExt, AsyncWriteExt, BufReader, Lines},
     net::TcpStream,
-    spawn,
     sync::mpsc,
 };
 
@@ -11,14 +10,21 @@ use eframe::egui;
 #[derive(Debug)]
 enum NetworkTaskCmd {
     Connect,
-    Send(UplinkMessage),
     Disconnect,
+    Send(UplinkMessage),
 }
 
-pub async fn network_task(
+#[derive(Debug)]
+enum NetworkTaskEvent {
+    Connected,
+    Disconnected,
+    Recv(DownlinkMessage),
+}
+
+async fn network_task(
     addr: String,
     mut rx_cmd: mpsc::UnboundedReceiver<NetworkTaskCmd>,
-    tx_msg: mpsc::UnboundedSender<DownlinkMessage>,
+    tx_msg: mpsc::UnboundedSender<NetworkTaskEvent>,
 ) -> Result<(), std::io::Error> {
     use tokio::net::tcp::{OwnedReadHalf, OwnedWriteHalf};
 
@@ -39,6 +45,7 @@ pub async fn network_task(
                 // Only react to commands
                 while let Some(cmd) = rx_cmd.recv().await {
                     match cmd {
+                        // GUI ask to connect
                         NetworkTaskCmd::Connect => match TcpStream::connect(&addr).await {
                             Ok(stream) => {
                                 let (r, w) = stream.into_split();
@@ -46,6 +53,7 @@ pub async fn network_task(
                                     writer: w,
                                     lines: BufReader::new(r).lines(),
                                 };
+                                tx_msg.send(NetworkTaskEvent::Connected).unwrap();
                                 break;
                             }
                             Err(e) => {
@@ -65,9 +73,13 @@ pub async fn network_task(
                     cmd = rx_cmd.recv() => {
                         match cmd {
                             Some(NetworkTaskCmd::Disconnect) => {
+                                // GUI ask to disconnect
+                                writer.shutdown().await.unwrap();
                                 state = State::Disconnected;
+                                tx_msg.send(NetworkTaskEvent::Disconnected).unwrap();
                             }
                             Some(NetworkTaskCmd::Send(msg)) => {
+                                // GUI ask to send message
                                 let json = serde_json::to_string(&msg)?;
                                 writer.write_all(json.as_bytes()).await?;
                                 writer.write_all(b"\n").await?;
@@ -83,14 +95,27 @@ pub async fn network_task(
                     result = lines.next_line() => {
                         match result {
                             Ok(Some(s)) => {
-                                if let Ok(msg) = serde_json::from_str::<DownlinkMessage>(&s) {
-                                    tx_msg.send(msg).unwrap();
+                                match serde_json::from_str::<DownlinkMessage>(&s) {
+                                    Ok(msg) => {
+                                        tx_msg.send(NetworkTaskEvent::Recv(msg)).unwrap();
+                                    }
+                                    Err(e) => {
+                                        eprintln!("invalid json: {e}, line={s}");
+                                    }
                                 }
                             },
-                            _ => {
+                            Ok(None) => {
+                                eprintln!("server closed connection");
                                 state = State::Disconnected;
+                                tx_msg.send(NetworkTaskEvent::Disconnected).unwrap();
                                 continue;
-                            },
+                            }
+                            Err(e) => {
+                                eprintln!("read error: {e}");
+                                state = State::Disconnected;
+                                tx_msg.send(NetworkTaskEvent::Disconnected).unwrap();
+                                continue;
+                            }
                         }
 
                     }
@@ -102,10 +127,15 @@ pub async fn network_task(
     Ok(())
 }
 
+#[derive(Default, Debug, Clone)]
+struct LobbyState {
+    pub chat_input: String,
+}
+
 #[derive(Debug, Clone)]
 enum ViewState {
     Home,
-    Lobby,
+    Lobby(LobbyState),
     Room(RoomId),
 }
 
@@ -115,6 +145,16 @@ struct Pending {
     description: String,
 }
 
+#[derive(Debug)]
+enum UiAction {
+    Connect,
+    Disconnect,
+    SendLobbyChat(String),
+    // future:
+    // EnterRoom(RoomId),
+    // CreateRoom,
+}
+
 pub struct App {
     state: ViewState,
     pending: Option<Pending>,
@@ -122,15 +162,15 @@ pub struct App {
     client_id: Option<ClientId>,
 
     tx_cmd: mpsc::UnboundedSender<NetworkTaskCmd>,
-    rx_msg: mpsc::UnboundedReceiver<DownlinkMessage>,
+    rx_msg: mpsc::UnboundedReceiver<NetworkTaskEvent>,
 
     chat_log: Vec<String>,
 }
 
 impl App {
-    pub fn new(
+    fn new(
         tx_cmd: mpsc::UnboundedSender<NetworkTaskCmd>,
-        rx_msg: mpsc::UnboundedReceiver<DownlinkMessage>,
+        rx_msg: mpsc::UnboundedReceiver<NetworkTaskEvent>,
     ) -> Self {
         Self {
             state: ViewState::Home,
@@ -191,7 +231,7 @@ impl App {
                         if self.pending_matches(req_id) {
                             self.pending = None;
                             if success {
-                                self.state = ViewState::Lobby;
+                                self.state = ViewState::Lobby(LobbyState::default());
                             }
                         }
                     }
@@ -231,31 +271,60 @@ impl App {
 }
 
 impl eframe::App for App {
-    fn ui(&mut self, ui: &mut egui::Ui, frame: &mut eframe::Frame) {
+    fn ui(&mut self, ui: &mut egui::Ui, _frame: &mut eframe::Frame) {
+        // -------------------------
         // 1. Drain server messages
+        // -------------------------
         while let Ok(msg) = self.rx_msg.try_recv() {
-            self.handle_server_msg(msg);
+            match msg {
+                NetworkTaskEvent::Connected => {}
+                NetworkTaskEvent::Disconnected => {
+                    self.state = ViewState::Home;
+                }
+                NetworkTaskEvent::Recv(downlink_message) => {
+                    self.handle_server_msg(downlink_message);
+                }
+            }
         }
 
-        // 2. UI
-        egui::CentralPanel::default().show_inside(ui, |ui| {
-            ui.heading("Client");
+        // -------------------------
+        // 2. UI → collect action
+        // -------------------------
+        let mut action: Option<UiAction> = None;
 
+        egui::CentralPanel::default().show_inside(ui, |ui| {
             if let Some(p) = &self.pending {
                 ui.label(format!("Pending: {}", p.description));
             }
 
-            match self.state {
+            match &mut self.state {
                 ViewState::Home => {
                     ui.heading("Home");
+                    ui.separator();
+
                     if ui.button("Connect").clicked() {
-                        self.connect();
+                        action = Some(UiAction::Connect);
                     }
                 }
-                ViewState::Lobby => {
+
+                ViewState::Lobby(lobby_state) => {
                     ui.heading("Lobby");
                     ui.separator();
-                    ui.heading("Chat");
+                    ui.label(format!("client_id: {}", self.client_id.unwrap()));
+                    if ui.button("Disconnect").clicked() {
+                        action = Some(UiAction::Disconnect);
+                    }
+
+                    ui.label("Chat");
+
+                    let response = ui.text_edit_singleline(&mut lobby_state.chat_input);
+
+                    if response.lost_focus() && ui.input(|i| i.key_pressed(egui::Key::Enter)) {
+                        let content = lobby_state.chat_input.clone();
+                        lobby_state.chat_input.clear();
+
+                        action = Some(UiAction::SendLobbyChat(content));
+                    }
 
                     for line in &self.chat_log {
                         ui.label(line);
@@ -264,11 +333,46 @@ impl eframe::App for App {
 
                 ViewState::Room(id) => {
                     ui.label(format!("You are in Room {}", id));
+
+                    // future action:
+                    // if ui.button("Leave").clicked() {
+                    //     action.push(UiAction::LeaveRoom);
+                    // }
                 }
             }
         });
 
-        ui.request_repaint(); // keep UI responsive
+        // -------------------------
+        // 3. Execute action
+        // -------------------------
+        if let Some(action) = action {
+            match action {
+                UiAction::Connect => {
+                    self.connect();
+                }
+
+                UiAction::Disconnect => {
+                    self.disconnect();
+                }
+
+                UiAction::SendLobbyChat(content) => {
+                    let req_id = self.next_req();
+
+                    self.send(
+                        UplinkMessage {
+                            client_id: self.client_id.unwrap(),
+                            req_id,
+                            msg: server::common::UplinkMessageValue::Lobby(
+                                server::common::UplinkLobbyMessage::Chat { content },
+                            ),
+                        },
+                        "Send LobbyChat",
+                    );
+                }
+            }
+        }
+
+        ui.request_repaint();
     }
 }
 
@@ -277,7 +381,7 @@ async fn main() {
     let (tx_cmd, rx_cmd) = mpsc::unbounded_channel();
     let (tx_msg, rx_msg) = mpsc::unbounded_channel();
 
-    spawn(network_task("127.0.0.1:8080".to_string(), rx_cmd, tx_msg));
+    tokio::spawn(network_task("127.0.0.1:8080".to_string(), rx_cmd, tx_msg));
 
     let options = eframe::NativeOptions::default();
     eframe::run_native(
